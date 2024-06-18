eda0f4fcb64b478983057662e7927238fc933d64
Gradi <56418645+gradikay@users.noreply.github.com>
2024-06-18 00:30:20 -0400

    commit via vs

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import '../App.css';
 
 export default function ImageCard(props){
     const {image} = props;
@@ -9,9 +10,9 @@ export default function ImageCard(props){
                 className="card-img-top px-4" 
                 alt="..."
             />
-          <div className="card-body pb-0" >
+          <div className="card-body py-0" >
             <h5 className="card-title">
-                <strong className="" style={{fontSize:"1.5rem"}}> 10 Scandalous Love Triangles Captivating the Public </strong>
+                <strong className="journal-title rounded shadow-sm" style={{fontSize:"1.5rem"}}> 10 Scandalous Love Triangles Captivating the Public </strong>
             </h5>
           </div>
           <ul class="list-group list-group-flush border-0">
